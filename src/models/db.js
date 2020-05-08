016df15ceb471923462eaaee35ef50676e82f29d
Oluwaseyi Joshua <seyijosh44@gmail.com>
2020-05-09 00:33:38 +0100

    External DB config added

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 export default {
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "food_project",
+  host: "https://databases.000webhost.com/",
+  databse: "id12466367_food_project",
+  user: "id12466367_nandar",
+  password: "0luw@seyi44J",
 };
