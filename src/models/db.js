f244be464f7167b129cf81508aa45189060dbb3e
Oluwaseyi Joshua <seyijosh44@gmail.com>
2020-06-30 21:05:47 +0100

    Added ClearDB config

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,10 @@
 export default {
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "food_project",
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "bfa043be50be43",
+  password: "8ad03da7",
+  database: "heroku_af0e7c041da29ba",
 };
+
+// mysql://bfa043be50be43:8ad03da7@us-cdbr-iron-east-05.cleardb.net/heroku_af0e7c041da29ba?reconnect=true
+
+// mysql -u nandara -p -h mysql.deligite.com food_project
